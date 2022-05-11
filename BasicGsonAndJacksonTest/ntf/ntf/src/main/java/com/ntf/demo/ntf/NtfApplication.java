04bc1112cc1c3ef36c549f235ca7ab446a3ba52f
ibrahimdoss <72415820+ibrahimdoss@users.noreply.github.com>
2022-05-11 23:35:52 +0300

    gson

---
+++ BasicGsonAndJacksonTest/ntf/ntf/src/main/java/com/ntf/demo/ntf/NtfApplication.java
@@ -0,0 +1,13 @@
+package com.ntf.demo.ntf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtfApplication.class, args);
+	}
+
+}
---
+++ BasicGsonAndJacksonTest/ntf/ntf/src/main/java/com/ntf/demo/ntf/test/GsonTest.java
@@ -0,0 +1,5 @@
+package com.ntf.demo.ntf.test;
+
+public class GsonTest {
+
+}
